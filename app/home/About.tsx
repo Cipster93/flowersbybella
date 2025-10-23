f9af069b8441f49a475d7947f2da34c486f9687e
Cipster93 <stroeflorinc@yahoo.com>
2025-10-23 22:07:54 +0300

    up

--- app/home/About.tsx
+++ app/home/About.tsx
@@ -21,6 +21,8 @@ export default function About() {
           alt="Flowers by Bella"
           className="object-cover rounded-xl shadow-2xl"
           fill
+          fetchPriority="high"
+          sizes="(max-width: 768px) 100vw, 50vw"
         />
       </section>
       <section
